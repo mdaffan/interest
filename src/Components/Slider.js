f242215e695a59af5e26c2848a51c9627e49bc14
Affan <mohammed.affan4@gmail.com>
2019-06-07 18:38:42 +0530

    Final Touches

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
-import Slider from '@material-ui/lab/Slider'
-import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 
 const styles = {
