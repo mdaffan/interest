f73ae018567b238847ddf10686da81d678bf6719
jitendra_moduluspi <jitendra.kumar@moduluspi.com>
2019-06-18 19:56:25 +0530

    ejecting

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+
 
 class SimpleSlider extends React.Component {
   constructor() {
@@ -10,26 +10,46 @@ class SimpleSlider extends React.Component {
       rate: '',
       payment: ''
     }
+    this.onChange=this.onChange.bind(this)
   }
-  async componentDidUpdate() {
-    const data = await fetch(
-      `https://ftl-frontend-test.herokuapp.com/interest?amount=${
-        this.state.sliderValue
-      }&numMonths=${this.state.months}`
-    )
-    const response = await data.json()
-    this.setState({
-      rate: response.interestRate,
-      payment: response.monthlyPayment.amount
-    })
+  async componentDidMount() {
+    // const data = await fetch(
+    //   `https://ftl-frontend-test.herokuapp.com/interest?amount=${
+    //     this.state.sliderValue
+    //   }&numMonths=${this.state.months}`
+    // )
+    // const response = await data.json()
+    // this.setState({
+    //   rate: response.interestRate,
+    //   payment: response.monthlyPayment.amount
+    // })
   }
 
   handleChange = (event, value) => {
     this.setState({ sliderValue: value })
   }
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
+  async onChange(e)  {
+ 
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+
+    if(!(e.target.value==="")){
+      const data = await fetch(
+        `https://ftl-frontend-test.herokuapp.com/interest?amount=${
+          this.state.sliderValue
+        }&numMonths=${this.state.months}`
+      )
+      const response = await data.json()
+      
+      this.setState({
+        
+        rate: response.interestRate,
+        payment: response.monthlyPayment.amount
+      })
+    }
+    
   }
 
   render() {
@@ -37,12 +57,12 @@ class SimpleSlider extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-lg-6 col-12 col-md-12">
-            <div class="input-group mb-3">
-              <div class="input-group-prepend">
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
                 <span className="input-group-text" style={{ marginTop: 40 }}>
                   Loan Amount
                 </span>
-                <span class="input-group-text" style={{ marginTop: 40 }}>
+                <span className="input-group-text" style={{ marginTop: 40 }}>
                   ${this.state.sliderValue}
                 </span>
               </div>
@@ -70,7 +90,7 @@ class SimpleSlider extends React.Component {
                 </span>
               </div>
 
-              <input
+              {/* <input
                 type="number"
                 name="months"
                 className="form-control"
@@ -78,9 +98,22 @@ class SimpleSlider extends React.Component {
                 style={{ marginTop: 40 }}
                 max={24}
                 value={this.state.months}
-                step={1}
+               
                 onChange={this.onChange}
                 aria-label="Amount (to the nearest dollar)"
+              /> */}
+              <input
+                type="range"
+                name="months"
+                style={{ marginTop: 40, paddingBottom: 17, paddingTop: 17 }}
+                className="custom-range form-control input-group-prepend"
+                id="customRange1"
+                onChange={this.onChange}
+                value={this.state.months}
+                step={1}
+                min={6}
+                max={24}
+                aria-labelledby="label"
               />
             </div>
           </div>
@@ -123,8 +156,5 @@ class SimpleSlider extends React.Component {
   }
 }
 
-SimpleSlider.propTypes = {
-  classes: PropTypes.object.isRequired
-}
 
 export default SimpleSlider
